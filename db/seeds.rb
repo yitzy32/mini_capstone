73a473d58c361fdd14bee27ad5e3539c2b6b94f5
Yitzy Bratt <yitzybratt32@gmail.com>
2021-02-23 20:34:04 -0500

    adds first product to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.new(name: "labtop", price: "350", img_
+  url: "www.xyz.com", description: "Lorem ibsem")
